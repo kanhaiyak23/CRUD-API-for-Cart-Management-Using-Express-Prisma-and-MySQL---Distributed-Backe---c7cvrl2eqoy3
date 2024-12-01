4ca89a30d008767fa08df6ac4300c7f44f50011d
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-12-01 18:39:24 +0530

    correct err message

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -2,7 +2,7 @@ const {validAuthKey} = require('./authKey')
 function AuthMiddleware(req,res,next){
     const apiKey = req.headers.apiauthkey
     if(!apiKey){
-        return res.status(403).json({error: 'Missing API Key'})
+        return res.status(403).json({error: 'apiauthkey is missing or invalid'})
     }
     if(apiKey !== validAuthKey){
         return res.status(403).json({error: 'Failed to authenticate apiauthkey'})
