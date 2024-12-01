06b854a00d39c94a5332e3268338741c8a6f1dd0
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-12-01 18:37:32 +0530

    complete

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -2,10 +2,10 @@ const {validAuthKey} = require('./authKey')
 function AuthMiddleware(req,res,next){
     const apiKey = req.headers.apiauthkey
     if(!apiKey){
-        return res.status(401).json({error: 'Missing API Key'})
+        return res.status(403).json({error: 'Missing API Key'})
     }
     if(apiKey !== validAuthKey){
-        return res.status(401).json({error: 'Failed to authenticate apiauthkey'})
+        return res.status(403).json({error: 'Failed to authenticate apiauthkey'})
     }
     next()
 }
